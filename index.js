1f425a1a1b19e820382dbcb959625f063c04e4ed
Gia Edgington <gia.edgington@gmail.com>
2019-12-03 09:45:28 -0500

    frontend connected to backend

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 
 document.addEventListener('DOMContentLoaded', function(){
+    var submitForm = document.querySelector(".submit");
+    var name = document.querySelector(".name");
+    var email = document.querySelector(".email");
+    var phone = document.querySelector(".phone");
+    var description = document.querySelector(".description");
+    var date = document.querySelector(".date");
     var slideIndex = 0;
     showSlides();
+
+    submitForm.addEventListener('click', function(){
+        
+        fetch('http://localhost:3000/contact', {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json",
+                "Accepts": "application/json"
+            },
+            body: JSON.stringify({
+                name: name.value,
+                email: email.value,
+                phone: phone.value,
+                case: description.value,
+                date: date.value
+            })
+        })
+        .then(response => response.json())
+        .then(console.log())
+    })
     
     function showSlides() {
         var i;
@@ -15,4 +41,7 @@ document.addEventListener('DOMContentLoaded', function(){
         slides[slideIndex-1].style.display = "block";  
         setTimeout(showSlides, 3000); // Change image every 2 seconds
     }
+
+    
+
 })
